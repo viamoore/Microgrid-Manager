877a35583b7af972b541f37127769851aabd4dcc
daxidngyn <daxidnguyen@gmail.com>
2024-01-30 22:23:12 -0800

    [feat] add new components + font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -55,6 +55,7 @@ module.exports = {
       },
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
+        display: ["Inter", "sans-serif"],
       },
       keyframes: {
         "accordion-down": {
