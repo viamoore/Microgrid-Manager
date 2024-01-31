fa792295bb2e5b6e1323cde633ad65ae0a0a5679
daxidngyn <daxidnguyen@gmail.com>
2024-01-31 00:43:26 -0800

    [feat] update device management page

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -3,7 +3,8 @@ import Page404 from "./page404/page404";
 
 // import Settings from "./configure/settings";
 import Settings from "./configure";
-import DeviceManagement from "./manageDevice/deviceManagement";
+// import DeviceManagement from "./manageDevice/deviceManagement";
+import DeviceManagement from "./manageDevice";
 import DataView from "./dataView/displayData";
 import LogPage from "./readLog/toLog";
 import CloudBackup from "./backupToCloud/cloudBackup";
